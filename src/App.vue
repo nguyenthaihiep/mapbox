d12cd57f6dee41d4b9319c5d7a8173db45e5ca90
Thai Hiep Nguyen <nguyenthaihiep94@gmail.com>
2021-06-10 12:16:51 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,47 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div id="map"></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+	import 'mapbox-gl/dist/mapbox-gl.css';
+	import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+	import mapboxgl from 'mapbox-gl';
+	import MapboxDirections from '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions';
+	
+	export default {
+		created() {
+			setTimeout(function() {
+				mapboxgl.accessToken = 'pk.eyJ1IjoiaGllcG5ndXllbjk0IiwiYSI6ImNrcHFiajN3cTBibDYyd2xlZnI2aTY4d2QifQ.aW648xC7HA6RDOKrRejAFg';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+				const map = new mapboxgl.Map({
+					container: 'map',
+					style: 'mapbox://styles/mapbox/streets-v11',
+					center: [106.6509628, 10.7323728],
+					zoom: 14
+				});
+
+				map.addControl(
+					new MapboxDirections({
+						accessToken: mapboxgl.accessToken
+					}),
+					'top-left'
+				);
+			},0);
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	* {
+		box-sizing: border-box;
+	}
+	body {
+		padding: 0;
+		margin: 0;
+	}
+	#map {
+		width: 100%;
+		height: 100vh;
+		display: inline-block;
+	}
 </style>
